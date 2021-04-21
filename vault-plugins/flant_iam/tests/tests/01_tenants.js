3ceccfee3127b25b9ea89fc0aabefbd7ede93247
Eugene Shevchenko <dev@takogo.net>
2021-04-21 14:17:43 +0300

    fmt with prettier

--- vault-plugins/flant_iam/tests/tests/01_tenants.js
+++ vault-plugins/flant_iam/tests/tests/01_tenants.js
@@ -1,59 +1,59 @@
-const { expect } = require("chai");
+const { expect } = require("chai")
 const {
-  Worder,
-  expectStatus,
-  getClient,
-  rootToken,
-  anotherToken,
-} = require("./lib");
+    Worder,
+    expectStatus,
+    getClient,
+    rootToken,
+    anotherToken,
+} = require("./lib")
 
-const TEN = "tenant";
+const TEN = "tenant"
 
 describe("Tenants", function () {
-  const root = getClient(rootToken);
-  const worder = new Worder();
+    const root = getClient(rootToken)
+    const worder = new Worder()
 
-  // afterEach("cleanup", async function () {
-  //     const clean = s => root.delete(`tenant/${s}`, expectStatus(204))
-  //     const promises = worder.list().map(clean)
-  //     await Promise.all(promises)
-  //     worder.clean()
-  // })
+    // afterEach("cleanup", async function () {
+    //     const clean = s => root.delete(`tenant/${s}`, expectStatus(204))
+    //     const promises = worder.list().map(clean)
+    //     await Promise.all(promises)
+    //     worder.clean()
+    // })
 
-  it("cannot be created without name", async () => {
-    await root.post(TEN, {}, { validateStatus: (s) => s >= 400 });
-  });
+    it("cannot be created without name", async () => {
+        await root.post(TEN, {}, { validateStatus: (s) => s >= 400 })
+    })
 
-  it("cannot be created with empty name", async () => {
-    await root.post(TEN, { name: "" }, expectStatus(400));
-  });
+    it("cannot be created with empty name", async () => {
+        await root.post(TEN, { name: "" }, expectStatus(400))
+    })
 
-  it("can be created with a name", async () => {
-    const payload = { name: worder.gen() };
-    const { data } = await root.post(TEN, payload, expectStatus(204));
-  });
+    it("can be created with a name", async () => {
+        const payload = { name: worder.gen() }
+        const { data } = await root.post(TEN, payload, expectStatus(204))
+    })
 
-  it("can be listed", async () => {
-    const payload = { name: worder.gen() };
-    await root.post(TEN, payload, expectStatus(204));
+    it("can be listed", async () => {
+        const payload = { name: worder.gen() }
+        await root.post(TEN, payload, expectStatus(204))
 
-    const { data } = await root.get("tenant?list=true", expectStatus(200));
+        const { data } = await root.get("tenant?list=true", expectStatus(200))
 
-    expect(data.data).to.be("array").and.to.be.not.empty;
-  });
+        expect(data.data).to.be("array").and.to.be.not.empty
+    })
 
-  it("have identifying fields in list", async () => {
-    const payload = { name: worder.gen() };
-    await root.post(TEN, payload, expectStatus(204));
+    it("have identifying fields in list", async () => {
+        const payload = { name: worder.gen() }
+        await root.post(TEN, payload, expectStatus(204))
 
-    const { data: body } = await root.get(
-      "tenant?list=true",
-      expectStatus(200)
-    );
+        const { data: body } = await root.get(
+            "tenant?list=true",
+            expectStatus(200),
+        )
 
-    expect(body.data).to.be("array").and.to.be.not.empty;
-    const tenant = body.data[0];
-    expect(tenant).to.include.keys("id", "name");
-    expect(tenant.name).to.eq(payload.name);
-  });
-});
+        expect(body.data).to.be("array").and.to.be.not.empty
+        const tenant = body.data[0]
+        expect(tenant).to.include.keys("id", "name")
+        expect(tenant.name).to.eq(payload.name)
+    })
+})
