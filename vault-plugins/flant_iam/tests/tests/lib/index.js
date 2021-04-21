bb52fa5d29e9214003e9ac9ea80dcb0624951663
Eugene Shevchenko <evgeny.shevchenko@flant.com>
2021-04-21 14:13:37 +0300

    add prettier

--- vault-plugins/flant_iam/tests/tests/lib/index.js
+++ vault-plugins/flant_iam/tests/tests/lib/index.js
@@ -1,79 +1,76 @@
-const A = require("axios")
-const F = require("faker")
-const fs = require("fs")
-const path = require("path")
+const A = require("axios");
+const F = require("faker");
+const fs = require("fs");
+const path = require("path");
 
-const baseUrl = "http://127.0.0.1:8200/v1/"
-const pluginPath = "flant_iam"
+const baseUrl = "http://127.0.0.1:8200/v1/";
+const pluginPath = "flant_iam";
 
 function getClient(token) {
-    const instance = A.create({
-        baseURL: baseUrl + pluginPath,
-        headers: {
-            "X-Vault-Token": token,
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-        }
-    })
+  const instance = A.create({
+    baseURL: baseUrl + pluginPath,
+    headers: {
+      "X-Vault-Token": token,
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  });
 
-    instance.interceptors.response.use(
-        null,
-        err => {
-            // Log and throw further
-            const sent = err.request.method + " " + err.request.path
-            const status = "Status " + err.response.status
-            const body = err.response.data ? JSON.stringify(err.response.data, null, 2) : ""
-            const msg = ["\n", sent, status, body].join("\n")
+  instance.interceptors.response.use(null, (err) => {
+    // Log and throw further
+    const sent = err.request.method + " " + err.request.path;
+    const status = "Status " + err.response.status;
+    const body = err.response.data
+      ? JSON.stringify(err.response.data, null, 2)
+      : "";
+    const msg = ["\n", sent, status, body].join("\n");
 
-            console.error(msg)
+    console.error(msg);
 
-            throw err
-        }
-    )
+    throw err;
+  });
 
-    return instance
+  return instance;
 }
-
 
 function expectStatus(expectedStatus) {
-    return {
-        validateStatus: x => x === expectedStatus
-    }
+  return {
+    validateStatus: (x) => x === expectedStatus,
+  };
 }
 
-
 class Worder {
-    constructor() {
-        this.set = new Set()
-    }
+  constructor() {
+    this.set = new Set();
+  }
 
-    gen(prefix = "") {
-        const word = F.lorem.word()
-        this.set.add(word)
-        // console.log("Worder gen", word)
-        return word
-    }
+  gen(prefix = "") {
+    const word = F.lorem.word();
+    this.set.add(word);
+    // console.log("Worder gen", word)
+    return word;
+  }
 
-    list() {
-        // console.log("Worder list", Array.from(this.set))
-        return Array.from(this.set)
-    }
+  list() {
+    // console.log("Worder list", Array.from(this.set))
+    return Array.from(this.set);
+  }
 
-    clean() {
-        // console.log("Worder clean")
-        this.set = new Set()
-    }
+  clean() {
+    // console.log("Worder clean")
+    this.set = new Set();
+  }
 }
 
 function getSecondRootToken() {
-    const token = fs.readFileSync(path.resolve("./data/token"))
-    return token.toString().trim()
+  const token = fs.readFileSync(path.resolve("./data/token"));
+  return token.toString().trim();
 }
 
 module.exports = {
-    getClient,
-    Worder,
-    expectStatus,
-    anotherToken: getSecondRootToken(),
-    rootToken: "root"
-}
+  getClient,
+  Worder,
+  expectStatus,
+  anotherToken: getSecondRootToken(),
+  rootToken: "root",
+};
