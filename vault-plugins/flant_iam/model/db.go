44463fb6599ff71766e8db305fe78f17476ff09f
Eugene Shevchenko <dev@takogo.net>
2021-04-29 13:33:47 +0300

    model: validate db schema on creation

--- vault-plugins/flant_iam/model/db.go
+++ vault-plugins/flant_iam/model/db.go
@@ -15,31 +15,48 @@ const (
 )
 
 func mergeSchema() (*memdb.DBSchema, error) {
-	schema := TenantSchema()
-	others := []*memdb.DBSchema{
+	included := []*memdb.DBSchema{
+		TenantSchema(),
 		UserSchema(),
 		ReplicaSchema(),
 		ProjectSchema(),
 		FeatureFlagSchema(),
 		ServiceAccountSchema(),
 		GroupSchema(),
-		RoleBindingSchema(),
 		RoleSchema(),
+		RoleBindingSchema(),
 	}
 
-	for _, o := range others {
-		for name, table := range o.Tables {
+	schema := &memdb.DBSchema{
+		Tables: map[string]*memdb.TableSchema{},
+	}
+
+	for _, s := range included {
+		for name, table := range s.Tables {
 			if _, ok := schema.Tables[name]; ok {
 				return nil, fmt.Errorf("table %q already there", name)
 			}
 			schema.Tables[name] = table
 		}
 	}
+
+	err := schema.Validate()
+	if err != nil {
+		return nil, err
+	}
 	return schema, nil
 }
 
 func GetSchema() (*memdb.DBSchema, error) {
-	return mergeSchema()
+	schema, err := mergeSchema()
+	if err != nil {
+		return nil, err
+	}
+	err = schema.Validate()
+	if err != nil {
+		return nil, err
+	}
+	return schema, nil
 }
 
 func NewResourceVersion() string {
