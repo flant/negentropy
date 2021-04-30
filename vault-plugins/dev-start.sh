dc61856b4ec5f06c1954df7c9e996f921a64ca5d
m.nabokikh <maksim.nabokikh@flant.com>
2021-04-30 13:01:32 +0400

    Use dev scripts to run e2e-tests
    Signed-off-by: m.nabokikh <maksim.nabokikh@flant.com>

--- vault-plugins/dev-start.sh
+++ vault-plugins/dev-start.sh
@@ -9,22 +9,22 @@ function activate_plugin() {
   plugin="$1"
 
   if [ $plugin == "flant_iam_auth" ]; then
-      docker-compose exec vault sh -c "vault auth enable -path=$plugin $plugin"
+      docker-compose exec -T vault sh -c "vault auth enable -path=$plugin $plugin"
   else
-      docker-compose exec vault sh -c "vault secrets enable -path=$plugin $plugin"
+      docker-compose exec -T vault sh -c "vault secrets enable -path=$plugin $plugin"
   fi
 
 
   if [ $plugin == "flant_iam" ]; then
-    docker-compose exec vault sh -c "vault write -force $plugin/kafka/generate_csr" >/dev/null 2>&1
-    docker-compose exec vault sh -c "vault write $plugin/kafka/configure_access kafka_endpoints=kafka:9092"
-    docker-compose exec vault sh -c "vault write flant_iam/kafka/configure self_topic_name=root_source"
+    docker-compose exec -T vault sh -c "vault write -force $plugin/kafka/generate_csr" >/dev/null 2>&1
+    docker-compose exec -T vault sh -c "vault write $plugin/kafka/configure_access kafka_endpoints=kafka:9092"
+    docker-compose exec -T vault sh -c "vault write flant_iam/kafka/configure self_topic_name=root_source"
   fi
 }
 
 specifiedPlugin=$1
 
-docker-compose exec vault sh -c "vault token create -orphan -policy=root -field=token > /vault/testdata/token"
+docker-compose exec -T vault sh -c "vault token create -orphan -policy=root -field=token > /vault/testdata/token"
 
 # single plugin build
 if [ -n "$specifiedPlugin" ]; then
