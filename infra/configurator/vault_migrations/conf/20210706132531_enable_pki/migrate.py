5a670c02b9708bf9deb9cf8f4a0a05fc44a36a7e
Vitaliy Snurnitsin <vitaliy.snurnitsin@gmail.com>
2021-07-06 14:22:24 +0300

    go

---
+++ infra/configurator/vault_migrations/conf/20210706132531_enable_pki/migrate.py
@@ -0,0 +1,24 @@
+"""
+This module contains a vault migration.
+Write your migration using hvac python module. See https://hvac.readthedocs.io/en/stable/overview.html for details.
+
+Migration Name: enable_pki
+Migration Version: 20210706132531
+"""
+import hvac
+
+def upgrade(connection):
+    # add your upgrade step here
+    # connection is an instance of hvac.Client()
+    # example code:
+    # print(connection.is_authenticated())
+    #
+    connection.sys.enable_secrets_engine(
+        backend_type='pki',
+        path='pki',
+    )
+    connection.sys.tune_mount_configuration(
+            path='pki',
+            max_lease_ttl='87600h',
+    )
+    pass
