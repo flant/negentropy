1fd14dabc648dc570aecf672dc3dbd8612b49ba0
Vitaliy Snurnitsin <vitaliy.snurnitsin@gmail.com>
2021-07-06 21:25:51 +0300

    go

--- infra/common/packer-scripts/99-sshd.sh
+++ infra/common/packer-scripts/99-sshd.sh
@@ -7,5 +7,6 @@ sed -i 's#ssh-keygen -A#mkdir -p /tmp/etc/ssh; ssh-keygen -A -f /tmp#g' /etc/ini
 # todo: uncomment ssh deletion
 #apk del openssh
 
+mkdir -p /root/.ssh
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDTXjTmx3hq2EPDQHWSJN7By1VNFZ8colI5tEeZDBVYAe9Oxq4FZsKCb1aGIskDaiAHTxrbd2efoJTcPQLBSBM79dcELtqfKj9dtjy4S1W0mydvWb2oWLnvOaZX/H6pqjz8jrJAKXwXj2pWCOzXerwk9oSI4fCE7VbqsfT4bBfv27FN4/Vqa6iWiCc71oJopL9DldtuIYDVUgOZOa+t2J4hPCCSqEJK/r+ToHQbOWxbC5/OAufXDw2W1vkVeaZUur5xwwAxIb3wM3WoS3BbwNlDYg9UB2D8+EZgNz1CCCpSy1ELIn7q8RnrTp0+H8V9LoWHSgh3VCWeW8C/MnTW90IR tfadm' > /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
