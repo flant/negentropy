0f5fd9ed71d98f8d50178e58b85a0a5e969fc223
Vitaliy Snurnitsin <vitaliy.snurnitsin@gmail.com>
2021-07-06 20:44:43 +0300

    google_storage_bucket force_destroy

--- infra/configurator/terraform/buckets.tf
+++ infra/configurator/terraform/buckets.tf
@@ -2,4 +2,6 @@ resource "google_storage_bucket" "vault" {
   for_each = toset(var.bucket_list)
   name     = each.value
   location = "EU"
+  # remove before deploy to production
+  force_destroy = true
 }
--- infra/main/terraform/main.tf
+++ infra/main/terraform/main.tf
@@ -121,4 +121,6 @@ resource "google_storage_bucket" "main" {
   for_each = toset(local.bucket_list)
   name     = each.value
   location = "EU"
+  # remove before deploy to production
+  force_destroy = true
 }
