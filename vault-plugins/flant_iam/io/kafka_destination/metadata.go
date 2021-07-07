8621ba47283e130485bec8bf29e77d00dc277505
Eugene Shevchenko <evgeny.shevchenko@flant.com>
2021-07-07 17:45:29 +0300

    add sa passwords to queues and the expectation to receive from them

--- vault-plugins/flant_iam/io/kafka_destination/metadata.go
+++ vault-plugins/flant_iam/io/kafka_destination/metadata.go
@@ -68,7 +68,6 @@ func (mkd *MetadataKafkaDestination) isValidObjectType(objType string) bool {
 	switch objType {
 	// TODO IdentitySharing
 	// TODO RoleBindingApproval
-	// TODO ServiceAccountPassword
 	case model.TenantType,
 		model.ProjectType,
 		model.UserType,
@@ -77,7 +76,8 @@ func (mkd *MetadataKafkaDestination) isValidObjectType(objType string) bool {
 		model.RoleType,
 		model.RoleBindingType,
 		model.GroupType,
-		model.MultipassType:
+		model.MultipassType,
+		model.ServiceAccountPasswordType:
 		return true
 
 	default:
