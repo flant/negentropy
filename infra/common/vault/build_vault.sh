72cea0d47cbb021b5b098fa21ffd2ec4a204b5cf
Artem Tuchinsky <artem.tuchinsky@flant.com>
2021-07-07 15:55:50 +0400

    fix vault build

--- infra/common/vault/build_vault.sh
+++ infra/common/vault/build_vault.sh
@@ -10,8 +10,10 @@ mkdir -p negentropy
 cp -R ../../../../vault-plugins negentropy
 patch -p1 < ../001_bucket_count.patch
 # patch -p1 < ../002_add_flant_plugins.patch
-go mod tidy
-go mod download
-go get github.com/mitchellh/gox
-CGO_ENABLED=1 XC_OS="linux" XC_ARCH="amd64" XC_OSARCH="linux/amd64" make dev
+##go mod tidy
+##go mod download
+##go get github.com/mitchellh/gox
+##CGO_ENABLED=1 XC_OS="linux" XC_ARCH="amd64" XC_OSARCH="linux/amd64" make dev
+make bootstrap
+make dev
 popd
