eec95840a36408aefd9c6f7de8e3945f533a5709
Ilya Lesikov <ilya@lesikov.com>
2021-07-07 11:59:35 +0300

    [flant_gitops] refactor configuration/validation

--- vault-plugins/flant_gitops/config.go
+++ vault-plugins/flant_gitops/config.go
@@ -1,8 +1,21 @@
 package flant_gitops
 
-const storageKeyConfigurationGitCredential = "configuration_git_credential"
+const (
+	storageKeyConfiguration              = "configuration"
+	storageKeyConfigurationGitCredential = "configuration_git_credential"
+)
 
 type gitCredential struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+type configuration struct {
+	GitRepoUrl                                 string `json:"git_repo_url"`
+	GitBranchName                              string `json:"git_branch_name"`
+	GitPollPeriod                              string `json:"git_poll_period"`
+	RequiredNumberOfVerifiedSignaturesOnCommit string `json:"required_number_of_verified_signatures_on_commit"`
+	InitialLastSuccessfulCommit                string `json:"initial_last_successful_commit"`
+	DockerImage                                string `json:"docker_image"`
+	Command                                    string `json:"command"`
 }
