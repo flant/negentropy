ad907adeacfa2fe60ecfbb8c505a6dca28eeef2b
Pavel Golovin <golovinps@yandex.ru>
2021-07-08 10:50:25 +0300

    build exit on error

--- infra/configurator/packer/build.sh
+++ infra/configurator/packer/build.sh
@@ -3,10 +3,12 @@
 for d in */ ; do
   echo "Building image from directory $d"
   output=$(cd "$d"; ./build.sh)
-  if [ $? -eq 0 ]; then
-    echo "Succeed: "
-  else
-    echo "Error occurred: "
-  fi
+  code=$?
   echo "$output"
+  if [ $code -eq 0 ]; then
+    echo "Succeed."
+  else
+    echo "Error occurred."
+    exit 1
+  fi
 done
