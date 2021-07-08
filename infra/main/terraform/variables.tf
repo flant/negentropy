a793d3fd2f842cdcab3fe9b657c56736d390f598
Vitaliy Snurnitsin <vitaliy.snurnitsin@gmail.com>
2021-07-08 15:37:15 +0300

    [terraform] add optional order static public ip

--- infra/main/terraform/variables.tf
+++ infra/main/terraform/variables.tf
@@ -24,6 +24,7 @@ locals {
     "instance_root_disk_size_gb" : "30"
     "instance_image" : "ubuntu-2004-focal-v20210415"
     "machine_type" : "n1-standard-2"
+    "order_public_static_ip" : false
     "ssh_user" : "user"
     "ssh_public_key" : "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDTXjTmx3hq2EPDQHWSJN7By1VNFZ8colI5tEeZDBVYAe9Oxq4FZsKCb1aGIskDaiAHTxrbd2efoJTcPQLBSBM79dcELtqfKj9dtjy4S1W0mydvWb2oWLnvOaZX/H6pqjz8jrJAKXwXj2pWCOzXerwk9oSI4fCE7VbqsfT4bBfv27FN4/Vqa6iWiCc71oJopL9DldtuIYDVUgOZOa+t2J4hPCCSqEJK/r+ToHQbOWxbC5/OAufXDw2W1vkVeaZUur5xwwAxIb3wM3WoS3BbwNlDYg9UB2D8+EZgNz1CCCpSy1ELIn7q8RnrTp0+H8V9LoWHSgh3VCWeW8C/MnTW90IR"
   }
