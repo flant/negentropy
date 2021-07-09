1e4b8754bcb33b57f27564904993c2e3b4345bf3
Pavel Golovin <golovinps@yandex.ru>
2021-07-09 17:34:22 +0300

    use alpine in docker (#88)

--- infra/common/docker/flant-gitops.sh
+++ infra/common/docker/flant-gitops.sh
@@ -2,4 +2,4 @@
 
 rm -rf /tmp/negentropy-gnupg-tmp
 mkdir -p /tmp/negentropy-gnupg-tmp
-docker run --pull always --cap-add=CAP_IPC_LOCK -it -w /app --rm -v /tmp/negentropy-gnupg-tmp:/tmp/gnupg -v $(pwd)/../../../:/app ubuntu:20.04 bash -c "cd infra/common/docker && source install-dependencies.sh && source env_vars && go version && ./apply.py --save-root-tokens-on-initialization --type $1"
+docker run --pull always --cap-add=CAP_IPC_LOCK -it -w /app --rm -v /tmp/negentropy-gnupg-tmp:/tmp/gnupg -v $(pwd)/../../../:/app alpine:3.13.5 sh -c "cd infra/common/docker && source install-dependencies.sh && source env_vars && go version && ./apply.py --save-root-tokens-on-initialization --type $1"
