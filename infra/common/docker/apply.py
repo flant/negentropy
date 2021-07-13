239ac908caf3e05baebe418756943ab5efa0a6a9
Artem Tuchinsky <artem.tuchinsky@flant.com>
2021-07-13 16:13:15 +0400

    add missing values to apply.py

--- infra/common/docker/apply.py
+++ infra/common/docker/apply.py
@@ -71,6 +71,11 @@ def main():
     os.environ['PKR_VAR_vault_conf_conf_bucket'] = "%s-vault-conf-conf" % google_project_id
     os.environ['PKR_VAR_vault_auth_bucket_trailer'] = "%s-vault-auth" % google_project_id
 
+    os.environ['PKR_VAR_vault_ca_name'] = "20210708-4qi-uyu"
+    os.environ['PKR_VAR_vault_ca_pool'] = "negentropy-flant-local"
+    os.environ['PKR_VAR_vault_ca_location'] = "negentropy.flant.local"
+    os.environ['PKR_VAR_vault_internal_root_domain'] = "europe-west1"
+
     os.environ['PKR_VAR_gcp_project'] = google_project_id
     os.environ['PKR_VAR_gcp_zone'] = "europe-west3-a"
 
@@ -97,6 +102,10 @@ vault_conf_bucket = "{os.environ.get('PKR_VAR_vault_conf_bucket')}"
 vault_conf_conf_bucket = "{os.environ.get('PKR_VAR_vault_conf_conf_bucket')}"
 # Will be used as "hostname.vault_auth_bucket_trailer".
 vault_auth_bucket_trailer = "{os.environ.get('PKR_VAR_vault_auth_bucket_trailer')}"
+vault_ca_name = "{os.environ.get('PKR_VAR_vault_ca_name')}"
+vault_ca_pool = "{os.environ.get('PKR_VAR_vault_ca_pool')}"
+vault_ca_location = "{os.environ.get('PKR_VAR_vault_ca_location')}"
+vault_internal_root_domain = "{os.environ.get('PKR_VAR_vault_internal_root_domain')}"
 # Variables to determine where are builder instances will run.
 ## Project will be also used for kafka CA request.
 gcp_project = "{os.environ.get('PKR_VAR_gcp_project')}"
