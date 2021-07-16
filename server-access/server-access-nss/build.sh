7041290f6e81c9e814235b4b1dc9ec4aa96e34e6
Yuriy Losev <losev.yur@gmail.com>
2021-07-16 19:51:20 +0400

    Update build.sh

--- server-access/server-access-nss/build.sh
+++ server-access/server-access-nss/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-echo TODO create script to build dynamic library for NSS.
-# go build ....
+echo "Comile and copy to /lib/x86_64-linux-gnu/libnss_flantauth.so.2"
+CGO_CFLAGS="-g -O2 -D __LIB_NSS_NAME=flantauth" go build  --buildmode=c-shared -o /lib/x86_64-linux-gnu/libnss_flantauth.so.2
