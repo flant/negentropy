6f7b5df8db3897216ca314e201f795d4da64525c
savelyev-an <savelyev_an@mail.ru>
2021-08-05 08:59:23 +0300

    add fixtures for multipass, group, project

---
+++ vault-plugins/flant_iam/fixtures/multipass.go
@@ -0,0 +1,28 @@
+package fixtures
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/flant/negentropy/vault-plugins/flant_iam/model"
+	"github.com/flant/negentropy/vault-plugins/flant_iam/uuid"
+)
+
+func RandomUserMultipassCreatePayload() map[string]interface{} {
+	multipass := model.Multipass{
+		// UUID:               "",
+		// TenantUUID:         "",
+		// OwnerUUID:          "",
+		// OwnerType:          "",
+		Description: "desc - " + uuid.New(),
+		TTL:         100 * time.Second,
+		MaxTTL:      1000 * time.Second,
+		CIDRs:       nil,
+		Roles:       nil,
+	}
+
+	bytes, _ := json.Marshal(multipass)
+	var payload map[string]interface{}
+	json.Unmarshal(bytes, &payload) //nolint:errcheck
+	return payload
+}
