0a4a384f31df28c6db1fa961fce7873227979d76
Artem Tuchinsky <artem.tuchinsky@flant.com>
2021-08-05 12:16:23 +0400

    ci: move e2e directory

---
+++ e2e/tests/crud/project/api_test.go
@@ -0,0 +1,19 @@
+package project
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/flant/negentropy/e2e/tests/lib"
+	"github.com/flant/negentropy/vault-plugins/flant_iam/backend/tests/specs/project"
+)
+
+func Test_tenantCRUD(t *testing.T) {
+	rootClient := lib.NewConfiguredIamVaultClient()
+	project.TenantAPI = lib.NewTenantAPI(rootClient)
+	project.TestAPI = lib.NewProjectAPI(rootClient)
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "CRUD: Project")
+}
---
+++ e2e/tests/crud/user/api_test.go
@@ -0,0 +1,20 @@
+package user
+
+import (
+	"testing"
+
+	"github.com/flant/negentropy/vault-plugins/flant_iam/backend/tests/specs/user"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/flant/negentropy/e2e/tests/lib"
+)
+
+func Test_userCRUD(t *testing.T) {
+	rootClient := lib.NewConfiguredIamVaultClient()
+	user.TenantAPI = lib.NewTenantAPI(rootClient)
+	user.TestAPI = lib.NewUserAPI(rootClient)
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "CRUD: User")
+}
