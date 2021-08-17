7a0f20832309209b09bbb19ab19408b7b0565885
savelyev-an <savelyev_an@mail.ru>
2021-08-17 16:43:06 +0300

    some refactoring

---
+++ cli/internals/consts/flags.go
@@ -0,0 +1,9 @@
+package consts
+
+const (
+	AllProjectsFlagName = "all-projects"
+	AllTenantsFlagName  = "all-tenants"
+	ProjectFlagName     = "project"
+	TenantFlagName      = "tenant"
+	LabelsFlagName      = "label"
+)
--- cli/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"main/cmd"
-)
-
-func main() {
-	cmd.Execute()
-}
