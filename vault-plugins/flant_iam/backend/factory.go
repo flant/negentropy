ff705c1ca6c88d3f9e7936e309ee12dc360a0641
savelyev-an <savelyev_an@mail.ru>
2021-09-16 09:44:41 +0300

    remove unuseful bigbadabum (#224)

--- vault-plugins/flant_iam/backend/factory.go
+++ vault-plugins/flant_iam/backend/factory.go
@@ -17,7 +17,6 @@ import (
 	"github.com/flant/negentropy/vault-plugins/flant_iam/io/kafka_source"
 	"github.com/flant/negentropy/vault-plugins/flant_iam/model"
 	iam_repo "github.com/flant/negentropy/vault-plugins/flant_iam/repo"
-	backentutils "github.com/flant/negentropy/vault-plugins/shared/backent-utils"
 	sharedio "github.com/flant/negentropy/vault-plugins/shared/io"
 	sharedjwt "github.com/flant/negentropy/vault-plugins/shared/jwt"
 	jwtkafka "github.com/flant/negentropy/vault-plugins/shared/jwt/kafka"
@@ -194,8 +193,6 @@ func newBackend(conf *logical.BackendConfig) (logical.Backend, error) {
 
 		extension_server_access.ServerPaths(b, storage, tokenController),
 		extension_server_access.ServerConfigurePaths(b, storage),
-
-		backentutils.BigBadabumPath(b),
 
 		tokenController.ApiPaths(),
 	)
