baa4f743790a4293d847a6f9af90ae2621d972cd
savelyev-an <savelyev_an@mail.ru>
2021-12-16 13:56:56 +0300

    make auth/tenants to returns all tenant info

--- vault-plugins/flant_iam_auth/usecase/tenant.go
+++ vault-plugins/flant_iam_auth/usecase/tenant.go
@@ -3,26 +3,21 @@ package usecase
 import (
 	iam "github.com/flant/negentropy/vault-plugins/flant_iam/model"
 	"github.com/flant/negentropy/vault-plugins/flant_iam/usecase"
-	"github.com/flant/negentropy/vault-plugins/flant_iam_auth/extensions/extension_server_access/model"
 	"github.com/flant/negentropy/vault-plugins/shared/consts"
 	"github.com/flant/negentropy/vault-plugins/shared/io"
 )
 
-func ListAvailableSafeTenants(txn *io.MemoryStoreTxn, acceptedTenants map[iam.TenantUUID]struct{}) ([]model.SafeTenant, error) {
+func ListAvailableUnSafeTenants(txn *io.MemoryStoreTxn, acceptedTenants map[iam.TenantUUID]struct{}) ([]iam.Tenant, error) {
 	tenants, err := usecase.Tenants(txn, consts.OriginAUTH).List(false)
 	if err != nil {
 		return nil, err
 	}
 
-	result := make([]model.SafeTenant, 0, len(tenants))
+	result := make([]iam.Tenant, 0, len(tenants))
 
 	for _, tenant := range tenants {
 		if _, accepted := acceptedTenants[tenant.UUID]; accepted {
-			res := model.SafeTenant{
-				UUID:    tenant.UUID,
-				Version: tenant.Version,
-			}
-			result = append(result, res)
+			result = append(result, *tenant)
 		}
 	}
 	return result, nil
