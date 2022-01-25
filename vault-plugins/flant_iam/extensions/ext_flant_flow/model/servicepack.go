6be53ea60df1087b9166a115ebe83008a3686a97
savelyev-an <savelyev_an@mail.ru>
2022-01-25 13:14:06 +0300

    service_pack repository

---
+++ vault-plugins/flant_iam/extensions/ext_flant_flow/model/servicepack.go
@@ -0,0 +1,25 @@
+package model
+
+import (
+	iam_model "github.com/flant/negentropy/vault-plugins/flant_iam/model"
+	"github.com/flant/negentropy/vault-plugins/shared/memdb"
+)
+
+const ServicePackType = "servicepack" // also, memdb schema name
+
+type ServicePack struct {
+	memdb.ArchiveMark
+
+	ProjectUUID  iam_model.ProjectUUID       `json:"uuid"`
+	Name         ServicePackName             `json:"service_pack_name"`
+	Version      string                      `json:"resource_version"`
+	Rolebindings []iam_model.RoleBindingUUID `json:"rolebindings"`
+}
+
+func (u *ServicePack) ObjType() string {
+	return ServicePackType
+}
+
+func (u *ServicePack) ObjId() string {
+	return u.ProjectUUID + "_" + u.Name
+}
