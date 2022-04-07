e0bf021352b6f4046436be0cc57e08b3b5e43302
savelyev-an <savelyev_an@mail.ru>
2022-04-07 08:37:43 +0300

    shared users works (#333)

--- e2e/tests/iam_crud/user/api_test.go
+++ e2e/tests/iam_crud/user/api_test.go
@@ -3,18 +3,19 @@ package user
 import (
 	"testing"
 
-	"github.com/flant/negentropy/vault-plugins/flant_iam/backend/tests/specs/user"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
 	"github.com/flant/negentropy/e2e/tests/lib"
+	"github.com/flant/negentropy/vault-plugins/flant_iam/backend/tests/specs/user"
 )
 
 func Test_userCRUD(t *testing.T) {
 	rootClient := lib.NewConfiguredIamVaultClient()
 	user.TenantAPI = lib.NewTenantAPI(rootClient)
 	user.TestAPI = lib.NewUserAPI(rootClient)
+	user.IdentitySharingAPI = lib.NewIdentitySharingAPI(rootClient)
+	user.GroupAPI = lib.NewGroupAPI(rootClient)
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "CRUD: User")
 }
