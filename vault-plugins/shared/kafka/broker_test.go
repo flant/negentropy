ffad5b3632a7aa7c4761c6830795ff49c448f66e
savelyev-an <savelyev_an@mail.ru>
2022-04-12 16:01:59 +0300

    tls config and use at e2e (#341)

--- vault-plugins/shared/kafka/broker_test.go
+++ vault-plugins/shared/kafka/broker_test.go
@@ -2,8 +2,6 @@ package kafka
 
 import (
 	"context"
-	"crypto/ecdsa"
-	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/rsa"
 	"encoding/json"
@@ -24,17 +22,13 @@ func TestMarshalingBrokerConfig(t *testing.T) {
 	})
 
 	t.Run("existent config", func(t *testing.T) {
-		priv, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-		require.NoError(t, err)
 		rpriv, err := rsa.GenerateKey(rand.Reader, 4096)
 		require.NoError(t, err)
 
 		storedConfig := BrokerConfig{
-			Endpoints:             []string{"localhost:9093", "localhost:9094"},
-			ConnectionPrivateKey:  priv,
-			ConnectionCertificate: nil,
-			EncryptionPrivateKey:  rpriv,
-			EncryptionPublicKey:   &rpriv.PublicKey,
+			Endpoints:            []string{"localhost:9093", "localhost:9094"},
+			EncryptionPrivateKey: rpriv,
+			EncryptionPublicKey:  &rpriv.PublicKey,
 		}
 
 		d2, err := json.Marshal(storedConfig)
@@ -45,8 +39,6 @@ func TestMarshalingBrokerConfig(t *testing.T) {
 		err = json.Unmarshal(d2, &newConfig)
 		require.NoError(t, err)
 		assert.Equal(t, storedConfig.Endpoints, newConfig.Endpoints)
-		assert.Equal(t, storedConfig.ConnectionCertificate, newConfig.ConnectionCertificate)
-		assert.Equal(t, storedConfig.ConnectionPrivateKey, newConfig.ConnectionPrivateKey)
 		assert.Equal(t, storedConfig.EncryptionPublicKey, newConfig.EncryptionPublicKey)
 		assert.Equal(t, storedConfig.EncryptionPrivateKey, newConfig.EncryptionPrivateKey)
 	})
