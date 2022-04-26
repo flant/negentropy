3de22ff8abecb265cbbdf1a68def4c69b72a2c8e
savelyev-an <savelyev_an@mail.ru>
2022-04-26 16:54:04 +0300

    add list_tenants and vst_owner vault plicies to each method (#351)

--- infra/main/vault_migrations/core/20220225173200_all_create_common_vault_policies/migrate.py
+++ infra/main/vault_migrations/core/20220225173200_all_create_common_vault_policies/migrate.py
@@ -15,3 +15,9 @@ def upgrade(vault_name: str, vaults: List[Vault]):
     print("INFO: create token renew policy at '{}' vault".format(vault_name))
     vault_client.sys.create_or_update_policy(name="token_renew",
                                              policy="""path "auth/token/lookup-self" {capabilities = ["create", "update", "read"]} path "auth/token/renew-self" {capabilities = ["create", "update", "read"]} """)
+    print("INFO: create vst_owner policy at '{}' vault".format(vault_name))
+    vault_client.sys.create_or_update_policy(name="vst_owner",
+                                             policy="""path "auth/flant_iam_auth/vst_owner" {capabilities = ["read"]}""")
+    print("INFO: create list_tenants policy at '{}' vault".format(vault_name))
+    vault_client.sys.create_or_update_policy(name="list_tenants",
+                                             policy="""path "auth/flant_iam_auth/tenant/" {capabilities = ["list"]}""")
