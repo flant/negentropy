d9ab1825348ad56821d14d13124435084a810913
savelyev-an <savelyev_an@mail.ru>
2022-08-12 14:18:04 +0300

    add tests for sets of servers

--- e2e/go.mod
+++ e2e/go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/onsi/ginkgo v1.16.5
 	github.com/onsi/gomega v1.20.0
 	github.com/tidwall/gjson v1.14.1
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa
 	gopkg.in/square/go-jose.v2 v2.6.0
 )
 
@@ -22,8 +23,11 @@ require (
 	github.com/Microsoft/go-winio v0.5.1 // indirect
 	github.com/OneOfOne/xxhash v1.2.8 // indirect
 	github.com/agnivade/levenshtein v1.0.1 // indirect
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
 	github.com/armon/go-metrics v0.3.10 // indirect
 	github.com/armon/go-radix v1.0.0 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
 	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
 	github.com/cenkalti/backoff/v3 v3.0.0 // indirect
 	github.com/confluentinc/confluent-kafka-go v1.9.1 // indirect
@@ -36,7 +40,17 @@ require (
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-chi/chi/v5 v5.0.7 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-openapi/analysis v0.21.2 // indirect
+	github.com/go-openapi/errors v0.20.2 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.20.0 // indirect
+	github.com/go-openapi/loads v0.21.1 // indirect
+	github.com/go-openapi/spec v0.20.6 // indirect
+	github.com/go-openapi/strfmt v0.21.3 // indirect
+	github.com/go-openapi/swag v0.21.1 // indirect
+	github.com/go-openapi/validate v0.22.0 // indirect
 	github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
@@ -64,6 +78,8 @@ require (
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/hashicorp/vault/sdk v0.5.3 // indirect
 	github.com/hashicorp/yamux v0.1.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mitchellh/copystructure v1.0.0 // indirect
@@ -74,6 +90,7 @@ require (
 	github.com/moby/term v0.0.0-20210619224110-3f7ff695adc6 // indirect
 	github.com/nxadm/tail v1.4.8 // indirect
 	github.com/oklog/run v1.0.0 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
 	github.com/open-policy-agent/opa v0.42.2 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.0.3-0.20211202183452-c5a74bcca799 // indirect
@@ -84,7 +101,6 @@ require (
 	github.com/ryanuber/go-glob v1.0.0 // indirect
 	github.com/sethvargo/go-password v0.2.0 // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stretchr/testify v1.8.0 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
@@ -92,8 +108,8 @@ require (
 	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/yashtewari/glob-intersection v0.1.0 // indirect
+	go.mongodb.org/mongo-driver v1.10.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
-	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
 	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
 	golang.org/x/oauth2 v0.0.0-20220722155238-128564f6959c // indirect
 	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
@@ -104,12 +120,14 @@ require (
 	google.golang.org/genproto v0.0.0-20220616135557-88e70c0c3a90 // indirect
 	google.golang.org/grpc v1.47.0 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/apimachinery v0.24.3 // indirect
 	k8s.io/klog/v2 v2.60.1 // indirect
 	k8s.io/utils v0.0.0-20220210201930-3a6ce19ff2f9 // indirect
+	sigs.k8s.io/yaml v1.3.0 // indirect
 )
 
 replace (
--- e2e/tests/ssh_open_internals/spec.go
+++ e2e/tests/ssh_open_internals/spec.go
@@ -6,15 +6,21 @@ import (
 	"crypto/sha256"
 	_ "embed"
 	"fmt"
+	"os"
+	"sort"
+	"strings"
 
 	vault_api "github.com/hashicorp/vault/api"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"golang.org/x/crypto/ssh"
 
+	"github.com/flant/negentropy/authd/pkg/daemon"
+	"github.com/flant/negentropy/cli/pkg"
 	"github.com/flant/negentropy/e2e/tests/lib"
 	"github.com/flant/negentropy/e2e/tests/lib/configure"
 	"github.com/flant/negentropy/e2e/tests/lib/flant_iam_preparing"
+	tsc "github.com/flant/negentropy/e2e/tests/lib/test_server_and_client_preparing"
 )
 
 var flantIamSuite flant_iam_preparing.Suite
@@ -31,23 +37,16 @@ var _ = BeforeSuite(func() {
 }, 1.0)
 
 var _ = Describe("Process of getting ssh access to server by a cli", func() {
-	It("direct sign certificate 30 times", func() {
-		for i := 0; i < 30; i++ {
+	It("direct sign certificate 10 times", func() {
+		for i := 0; i < 10; i++ {
+			pubkey, principals, serversUUIDs := pubkeyAndPrincipals(i)
+
 			var secret *vault_api.SecretAuth
-			secret = userDirectLoginForSignCert(cfg.TestServer.TenantUUID, cfg.TestServer.ProjectUUID, cfg.TestServer.UUID)
-
-			privateRSA, err := rsa.GenerateKey(rand.Reader, 2048)
-			Expect(err).ToNot(HaveOccurred())
-
-			pubkey, err := ssh.NewPublicKey(&privateRSA.PublicKey)
-			Expect(err).ToNot(HaveOccurred())
-
-			hash := sha256.Sum256([]byte(cfg.TestServer.UUID + cfg.User.UUID))
-			principal := fmt.Sprintf("%x", hash)
+			secret = userDirectLoginForSignCert(lib.GetAuthVaultUrl(), cfg.TestServer.TenantUUID, cfg.TestServer.ProjectUUID, serversUUIDs)
 
 			data := map[string]interface{}{
 				"public_key":       string(ssh.MarshalAuthorizedKey(pubkey)),
-				"valid_principals": principal,
+				"valid_principals": principals,
 			}
 
 			cl := configure.GetClientWithToken("", lib.GetAuthVaultUrl())
@@ -56,16 +55,79 @@ var _ = Describe("Process of getting ssh access to server by a cli", func() {
 
 			ssh := cl.SSHWithMountPoint("ssh")
 			s, err := ssh.SignKey("signer", data)
+
 			Expect(err).ToNot(HaveOccurred())
-			fmt.Printf("\n======================\n")
-			fmt.Printf("%#v\n", s)
+			fmt.Printf("principals: %s key: %#v\n", principals, s)
+		}
+	})
+
+	It("sign certificate 10 times through authd", func() {
+		stopChan := runAuthdDaemon()
+		defer func() {
+			stopChan <- struct{}{}
+		}()
+		curFolder, err := os.Getwd()
+		Expect(err).ToNot(HaveOccurred())
+
+		authdSocketPath := curFolder + "/" + "authd.sock"
+		os.Setenv("AUTHD_SOCKET_PATH", authdSocketPath)
+
+		for i := 0; i < 10; i++ {
+
+			cliVaultClient, err := pkg.DefaultVaultClient()
+			Expect(err).ToNot(HaveOccurred())
+
+			pubkey, principals, serversUUIDs := pubkeyAndPrincipals(i)
+
+			sshRequest := pkg.VaultSSHSignRequest{
+				PublicKey:       string(ssh.MarshalAuthorizedKey(pubkey)),
+				ValidPrincipals: principals,
+			}
+
+			s, err := cliVaultClient.SignPublicSSHCertificate(cfg.TestServer.TenantUUID, cfg.TestServer.ProjectUUID, serversUUIDs, sshRequest)
+
+			Expect(err).ToNot(HaveOccurred())
+			fmt.Printf("principals: %s key: %#v\n", principals, s)
 		}
 	})
 })
 
+// runAuthdDaemon returns chan for stopping
+func runAuthdDaemon() chan struct{} {
+	curFolder, err := os.Getwd()
+	Expect(err).ToNot(HaveOccurred())
+	err = os.WriteFile("client-jwt", []byte(cfg.UserMultipassJWT), 0600)
+	Expect(err).ToNot(HaveOccurred())
+
+	clientMainCfg := tsc.ClientMainAuthdCFG(&tsc.MainAuthdCfgV1{
+		DefaultSocketDirectory: curFolder,
+		JwtPath:                curFolder + "/" + "client-jwt",
+		RootVaultInternalURL:   lib.GetRootVaultUrl(),
+		AuthVaultInternalURL:   lib.GetAuthVaultUrl(),
+	})
+	err = os.WriteFile("main.yaml", []byte(clientMainCfg), 0666)
+	Expect(err).ToNot(HaveOccurred())
+
+	clientSocketCfg := tsc.ClientSocketCFG
+	err = os.WriteFile("sock1.yaml", []byte(clientSocketCfg), 0777)
+	Expect(err).ToNot(HaveOccurred())
+
+	authd := daemon.NewDefaultAuthd()
+	authd.Config.ConfDirectory = curFolder
+	err = authd.Start()
+	Expect(err).ToNot(HaveOccurred())
+
+	stopChan := make(chan struct{})
+	go func() {
+		<-stopChan
+		authd.Stop()
+	}()
+	return stopChan
+}
+
 const SSHOpenRole = "ssh.open"
 
-func userDirectLoginForSignCert(tenantUUID, projectUUID, serverUUID string) *vault_api.SecretAuth {
+func userDirectLoginForSignCert(vaultApiUrl, tenantUUID, projectUUID string, serverUUIDs []string) *vault_api.SecretAuth {
 	params := map[string]interface{}{
 		"method": "multipass",
 		"jwt":    cfg.UserMultipassJWT,
@@ -77,17 +139,40 @@ func userDirectLoginForSignCert(tenantUUID, projectUUID, serverUUID string) *vau
 				"claim": map[string]interface{}{
 					"ttl":     "720m",
 					"max_ttl": "1440m",
-					"servers": []string{serverUUID},
+					"servers": serverUUIDs,
 				},
 			},
 		}}
 
-	cl := configure.GetClientWithToken("", lib.GetAuthVaultUrl())
-	cl.ClearToken()
+	cl := configure.GetClientWithToken("", vaultApiUrl)
 
 	secret, err := cl.Logical().Write(lib.IamAuthPluginPath+"/login", params)
 	Expect(err).ToNot(HaveOccurred())
 	Expect(secret).ToNot(BeNil())
 	Expect(secret.Auth).ToNot(BeNil())
 	return secret.Auth
+}
+
+// testServers provides different cases for testing
+func testServers(i int) []string {
+	set := [][]string{{cfg.TestServer.UUID}, {cfg.TestServer2.UUID}, {cfg.TestServer.UUID, cfg.TestServer2.UUID}, {cfg.TestServer2.UUID, cfg.TestServer.UUID}}
+	return set[i%4]
+}
+
+func pubkeyAndPrincipals(i int) (ssh.PublicKey, string, []string) {
+	privateRSA, err := rsa.GenerateKey(rand.Reader, 2048)
+	Expect(err).ToNot(HaveOccurred())
+
+	pubkey, err := ssh.NewPublicKey(&privateRSA.PublicKey)
+	Expect(err).ToNot(HaveOccurred())
+
+	serverUUIDs := testServers(i)
+	principals := []string{}
+	for _, serverUUID := range serverUUIDs {
+		hash := sha256.Sum256([]byte(serverUUID + cfg.User.UUID))
+		principals = append(principals, fmt.Sprintf("%x", hash))
+	}
+	sort.Strings(principals)
+
+	return pubkey, strings.Join(principals, ","), serverUUIDs
 }
