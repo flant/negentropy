5ee29f09987e0774e14fcf10c1eb346bf76fca7f
savelyev-an <savelyev_an@mail.ru>
2022-12-19 13:23:20 +0300

    implement cleaning teams&teammates

--- e2e/tests/lib/item_watcher/internal/cleaner.go
+++ e2e/tests/lib/item_watcher/internal/cleaner.go
@@ -2,12 +2,16 @@ package internal
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
-	"github.com/flant/negentropy/e2e/tests/lib"
+	"net/http"
+	"net/url"
+	"strings"
+
 	"github.com/hashicorp/go-multierror"
 	"github.com/hashicorp/vault/api"
-	"net/http"
-	"strings"
+
+	"github.com/flant/negentropy/e2e/tests/lib"
 )
 
 const AuthPluginSelfTopic = "authplugin_self_topic"
@@ -25,7 +29,6 @@ func CleanTopic(summary *SummaryOfTopic) ([]string, error) {
 	default:
 		return nil, fmt.Errorf("topic with type %q is not cleanable", summary.Topic.Type)
 	}
-	return nil, nil
 }
 
 func cleanAuthSelfTopic(summary *SummaryOfTopic) ([]string, error) {
@@ -56,14 +59,26 @@ func cleanTopic(summary *SummaryOfTopic, itemTypes []Type, erasers []func(vaultC
 
 func cleanItems(client *api.Client, items map[ItemKey]ItemSummary, itemEraser func(vaultClient *api.Client, item ItemSummary) error) (int, error) {
 	count := 0
-	for _, summary := range items {
-		if !summary.IsDeleted {
+	for {
+		nextItems := map[ItemKey]ItemSummary{}
+		if len(items) == 0 {
+			break
+		}
+		for k, summary := range items {
+			if summary.IsDeleted {
+				continue
+			}
 			err := itemEraser(client, summary)
+			if errors.Is(err, errRepeatLater) {
+				nextItems[k] = summary
+				continue
+			}
 			if err != nil {
-				return count, err
+				panic(err)
 			}
 			count++
 		}
+		items = nextItems
 	}
 	return count, nil
 }
@@ -121,14 +136,14 @@ func erasePolicy(vaultClient *api.Client, policySummary ItemSummary) error {
 
 // cleanIamSelfTopic
 func cleanIamSelfTopic(summary *SummaryOfTopic) ([]string, error) {
-	itemTypes := []Type{"tenant"}                                       // what itemTypes should be deleted
+	itemTypes := []Type{"tenant", "teammate"}                           // what itemTypes should be deleted
 	erasers := []func(vaultClient *api.Client, item ItemSummary) error{ // should suit itemTypes
-		eraseTenant,
+		eraseWholeTenant, eraseWholeTeam,
 	}
 	return cleanTopic(summary, itemTypes, erasers)
 }
 
-func eraseTenant(vaultClient *api.Client, tenantSummary ItemSummary) error {
+func eraseWholeTenant(vaultClient *api.Client, tenantSummary ItemSummary) error {
 	tenantKey := tenantSummary.Key // tenant/{tenant_uuid}
 	basePath := "flant/" + tenantKey
 	resp, err := vaultClient.Logical().Read(basePath)
@@ -136,6 +151,10 @@ func eraseTenant(vaultClient *api.Client, tenantSummary ItemSummary) error {
 		return err
 	}
 	tenantRaw := resp.Data["tenant"].(map[string]interface{})
+	if tenantRaw["origin"] == "flant_flow_predefined" {
+		// skip, can't delete
+		return nil
+	}
 	if tenantRaw["origin"] == "flant_flow" {
 		splitted := strings.Split(tenantKey, "/")
 		basePath = "flant/client/" + splitted[1]
@@ -148,4 +167,97 @@ func eraseTenant(vaultClient *api.Client, tenantSummary ItemSummary) error {
 	}
 	_, err = vaultClient.Logical().Delete(basePath + "/erase")
 	return err
+}
+
+var errRepeatLater = fmt.Errorf("object is linked to some object the same type, repeat later")
+
+func eraseWholeTeam(vaultClient *api.Client, teamSummary ItemSummary) error {
+	err := checkCanBeDeleted(vaultClient, teamSummary.Key)
+	if err != nil {
+		return err
+	}
+
+	request := vaultClient.NewRequest("GET", "/v1/flant/"+teamSummary.Key+"/teammate")
+	request.URL.Path = request.URL.Path + "/"
+	request.Params = url.Values{"show_archived": {"true"}}
+
+	resp, err := vaultClient.RawRequest(request)
+	if err != nil {
+		return err
+	}
+	secret, err := api.ParseSecret(resp.Body)
+	teammatesRaw := secret.Data["teammates"].([]interface{})
+	for _, teammateRaw := range teammatesRaw {
+		teammate := teammateRaw.(map[string]interface{})
+		teammateUUID := teammate["uuid"].(string)
+		basePath := "flant/" + teamSummary.Key + "/teammate/" + teammateUUID
+		if teammate["archiving_timestamp"] == json.Number("0") {
+			_, err = vaultClient.Logical().Delete(basePath)
+			if err != nil {
+				return err
+			}
+		}
+		_, err = vaultClient.Logical().Delete(basePath + "/erase")
+		if err != nil {
+			return err
+		}
+	}
+
+	secret, err = vaultClient.Logical().Read("flant/" + teamSummary.Key)
+	if err != nil {
+		return err
+	}
+	teamRaw := secret.Data["team"].(map[string]interface{})
+	if teamRaw["archiving_timestamp"] == json.Number("0") {
+		_, err = vaultClient.Logical().Delete("flant/" + teamSummary.Key)
+		if err != nil {
+			return err
+		}
+	}
+	_, err = vaultClient.Logical().Delete("flant/" + teamSummary.Key + "/erase")
+	return err
+}
+
+func checkCanBeDeleted(vaultClient *api.Client, teamKafkaKey string) error {
+	teamUUID := extractID(teamKafkaKey)
+	println(teamUUID)
+	request := vaultClient.NewRequest("GET", "/v1/flant/team")
+	request.URL.Path = request.URL.Path + "/"
+	request.Params = url.Values{"show_archived": {"true"}}
+
+	resp, err := vaultClient.RawRequest(request)
+	if err != nil {
+		return err
+	}
+	secret, err := api.ParseSecret(resp.Body)
+	if err != nil {
+		return err
+	}
+	found := false // TODO REMOVE
+	teamsRaw := secret.Data["teams"].([]interface{})
+	for _, teamRaw := range teamsRaw {
+		checkTeam := teamRaw.(map[string]interface{})
+		checkTeamIdentifier := checkTeam["identifier"].(string)
+		checkTeamUUID := checkTeam["uuid"].(string)
+		checkParentTeam := checkTeam["parent_team_uuid"].(string)
+		if checkParentTeam == teamUUID {
+			return fmt.Errorf("%w: %q is parent for %q team", errRepeatLater, teamUUID, checkTeamIdentifier)
+		}
+		if teamUUID == checkTeamUUID { // TODO REMOVE
+			found = true
+		}
+	}
+	if !found {
+		return fmt.Errorf("%q team is erased", teamUUID)
+	}
+	return nil
+
+}
+
+func extractID(kafkeKey string) string {
+	splitted := strings.Split(kafkeKey, "/")
+	if len(splitted) != 2 {
+		panic(fmt.Sprintf("wrong key to extract ID: %s", kafkeKey))
+	}
+	return splitted[1]
 }
